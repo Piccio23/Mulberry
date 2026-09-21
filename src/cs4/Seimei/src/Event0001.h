aeee7ce000376e8922d66fe8e7b80266b8e3d71a
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-09-21 22:28:43 +0200

    update `Event0001.h`

--- src/cs4/Seimei/src/Event0001.h
+++ src/cs4/Seimei/src/Event0001.h
@@ -2,7 +2,11 @@
 #define EVENT0001_H
 
 #include "typedefs.h"
+#include "Event.h"
 
+void ActiS0001Event000(EVENT* lpEvent, u_int Id);
+void PlayS0001Event000(EVENT* lpEvent);
+void InitS0001Event000(EVENT* lpEvent);
 void SetEventFuncS0001(void);
 
 #endif // EVENT0001_H
